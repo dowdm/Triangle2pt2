11cdbaf1d1f46bf44c248570946aa27a9532e9e3
Mike Chu and Matt Dowdney <mike@>
2018-04-17 10:46:45 -0700

    Refactor Triangle project JS into backend file and pass unit tests.

--- src/main.js
+++ src/main.js
@@ -1,27 +1,30 @@
-// import { function } from './filename';
+import $ from 'jquery';
+import { Triangle } from './triangle.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-// import $ from 'jquery';
 
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-    var side1 = parseInt($("#side1").val());
-    var side2 = parseInt($("#side2").val());
-    var side3 = parseInt($("#side3").val());
+    var inputSide1 = parseInt($("#side1").val());
+    var inputSide2 = parseInt($("#side2").val());
+    var inputSide3 = parseInt($("#side3").val());
 
-    if (side1 + side2 <= side3) {
-      $("#result").text("This is not a triangle.");
-    } else if (side1 === side2 && side2 === side3) {
-      $("#result").text("This is an equilateral triangle.");
-    } else if (side1 === side2 || side2 === side3 ||side1 === side3) {
-      $("#result").text("This is an isosceles triangle.")
-    } else {
-      $("#result").text("This is a scalene triangle.")
-    }
+    var newTriangle = new Triangle(inputSide1, inputSide2, inputSide3);
 
+    // if (side1 + side2 <= side3) {
+    //   $("#result").text("This is not a triangle.");
+    // } else if (side1 === side2 && side2 === side3) {
+    //   $("#result").text("This is an equilateral triangle.");
+    // } else if (side1 === side2 || side2 === side3 ||side1 === side3) {
+    //   $("#result").text("This is an isosceles triangle.")
+    // } else {
+    //   $("#result").text("This is a scalene triangle.")
+    // }
+
+    $("#result").text(newTriangle.checkType());
     $("#result").show();
     $("form")[0].reset();
   });
